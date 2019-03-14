a29114e8b21b2bbaf85c16d02203272c83e573af
Thatawesomeguy4 <derekteepee@gmail.com>
2019-03-14 14:41:07 -0400

    uploaded code that plays a sound

---
+++ sound code/BrainTunerMedia.cs
@@ -0,0 +1,39 @@
+﻿using Android.Content.Res;
+using Plugin.SimpleAudioPlayer;
+using Android.Content;
+
+namespace FiddleYourself
+{
+    class BrainTunerMedia
+    {
+
+        public ISimpleAudioPlayer GPlayer;
+        public ISimpleAudioPlayer DPlayer;
+        public ISimpleAudioPlayer APlayer;
+        public ISimpleAudioPlayer EPlayer;
+        public Context content;
+
+        //default constructor to load audio assets
+        public BrainTunerMedia()
+        {
+            //create audio players
+            this.GPlayer = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            this.DPlayer = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            this.APlayer = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            this.EPlayer = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+
+            //get the local context into our variable
+            this.content = Android.App.Application.Context;
+
+            //create asset manager
+            AssetManager assetManager = this.content.Assets;
+
+            //load audio files using the asset manager
+            this.GPlayer.Load(assetManager.Open("Gravity Falls.mp3"));
+            this.DPlayer.Load(assetManager.Open("Gravity Falls.mp3"));
+            this.APlayer.Load(assetManager.Open("Gravity Falls.mp3"));
+            this.EPlayer.Load(assetManager.Open("Gravity Falls.mp3"));
+        }
+
+    }
+}
---
+++ sound code/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FiddleYourself
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //init media
+        BrainTunerMedia brainTuner = new BrainTunerMedia();
+
+        int count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            brainTuner.APlayer.Play();
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}

4599e0fbe4193d40df3a3c88b482689d3d1fba48
TannerXD <34318548+TannerXD@users.noreply.github.com>
2019-03-25 18:33:22 -0400

    Tuning

---
+++ TuningActivity.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FidLinn
+{
+    [Activity(Label = "TuningActivity")]
+    public class TuningActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Tuning);
+
+
+            // Create your application here
+
+            Button backButton = FindViewById<Button>(Resource.Id.backButton);
+
+            backButton.Click += delegate { BackButtonHandler(); };
+        }
+        private void BackButtonHandler()
+        {
+            SetContentView(Resource.Layout.MainPageLayout);
+        }
+
+    }
+}
